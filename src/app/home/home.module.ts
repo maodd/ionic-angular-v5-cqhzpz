21f5f337f06a6b270ad0ee46f64bcfb7de848237
maodd <maodd.ca@gmail.com>
2021-05-10 15:02:59 -0600

    Initial commit

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+import { HomePageRoutingModule } from './home-routing.module';
+import { IonicSelectableModule } from '@ionic-selectable/angular';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    IonicSelectableModule
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,37 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonicSelectableComponent } from '@ionic-selectable/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss']
+})
+export class HomePage {
+  @ViewChild('ionicSelectable') ionicSelectable: IonicSelectableComponent;
+
+  items = [];
+  /*     { id: 1, port: 'Salina Cruz', country: { country: 'Mexico' } },
+    { id: 2, port: 'Valencia', country: { country: 'Spain' } },
+    { id: 3, port: 'Veracruz', country: { country: 'Mexico' } }
+  ]; */
+
+  binding;
+
+  constructor() {
+    for (let index = 1; index < 20; index++) {
+      this.items.push({
+        id: index,
+        port: 'Salina Cruz',
+        country: { country: 'Mexico' }
+      });
+    }
+  }
+
+  onChanged(event: CustomEvent) {
+    console.log(event);
+  }
+
+  open() {
+    this.ionicSelectable.open();
+  }
+}
